83c4b4ba65cd2a7c0f6c5465df0baeb8dd7e7478
Chase Johnson <Chase.b.johnson@gmail.com>
2020-10-16 11:01:35 -0700

    Setup Script file.

---
+++ Script.py
@@ -0,0 +1,13 @@
+import pygame
+
+# Starts game 
+pygame.init()
+
+# Game display window 
+gameDisplay = game.display.set_mode((800,600))
+
+#updates game
+pygame.display.update()
+
+# quits game 
+pygame.quit()

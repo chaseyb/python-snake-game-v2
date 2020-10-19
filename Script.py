466aba784ca4a82a55a1e13e5cf1543f0fece262
Chase Johnson <Chase.b.johnson@gmail.com>
2020-10-18 21:26:53 -0700

    started defining key events.

--- Script.py
+++ Script.py
@@ -9,20 +9,32 @@ black = (0,0,0)
 # Game display window 
 gameDisplay = pygame.display.set_mode((800,600))
 
-#updates game
+# Updates game
 pygame.display.update()
 pygame.display.set_caption('Snake Game V2 by Chaseyb')
 
 gameExit = False
 
+lead_x = 300
+lead_y = 300
+
 while not gameExit:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             gameExit = True
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_LEFT:
+                lead_x -= 10
+            if event.key == pygame.K_RIGHT:
+                lead_x =+ 10
 
+
+    # Defines game background
     gameDisplay.fill(white)
-    pygame.draw.rect(gameDisplay, black, [400, 300, 10, 10])
-
+    pygame.draw.rect(gameDisplay, black, [lead_x, Lead_y, 10, 10])
+    # Updates game display
     pygame.display.update() 
 
 
+pygame.quit()
+quit()

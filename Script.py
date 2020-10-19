ff68f955393a1cbd7e446d807931aa0104ae39c7
Chase Johnson <Chase.b.johnson@gmail.com>
2020-10-19 00:13:57 -0700

    cleaned up code.

--- Script.py
+++ Script.py
@@ -1,6 +1,6 @@
 import pygame
 import random
-import pickle  # pickle is used for high score saving
+import pickle
 
 # Starts game 
 pygame.init()
@@ -13,18 +13,19 @@ black = (0,0,0)
 display_width = 800
 display_height = 600
 blockSize = 20
+FPS = 30
 
 # Game display window 
 gameDisplay = pygame.display.set_mode((display_width,display_height))
 
 # Updates game
 pygame.display.update()
-pygame.display.set_caption('Snake Game V2 by Chaseyb')
+pygame.display.set_caption('Snake Game')
 
 gameExit = False
 
-lead_x = 300
-lead_y = 300
+lead_x = display_height/2
+lead_y = display_width/2
 
 lead_x_change = 0
 lead_y_change = 0
@@ -39,31 +40,31 @@ while not gameExit:
             gameExit = True
         if event.type == pygame.KEYDOWN:
             if event.key == pygame.K_LEFT:
-                lead_x_change = -10
+                lead_x_change = -blockSize
                 lead_y_change = 0
             elif event.key == pygame.K_RIGHT:
-                lead_x_change = 10
+                lead_x_change = blockSize
                 lead_y_change = 0
             elif event.key == pygame.K_UP:
-                lead_y_change = -10
+                lead_y_change = -blockSize
                 lead_x_change = 0
             elif event.key == pygame.K_DOWN:
-                lead_y_change = 10
+                lead_y_change = blockSize
                 lead_x_change = 0
 
     # Defines window boundary
-    if lead_x >= 800 or lead_x < 0 or lead_y >= 600 or lead_y < 0:
+    if lead_x >= display_width or lead_x < 0 or lead_y >= display_height or lead_y < 0:
         gameExit = True 
     
     lead_x += lead_x_change
     lead_y += lead_y_change
     # Defines game background
     gameDisplay.fill(white)
-    pygame.draw.rect(gameDisplay, black, [lead_x,lead_y,10,10])
+    pygame.draw.rect(gameDisplay, black, [lead_x,lead_y,blockSize,blockSize])
     # Updates game display
     pygame.display.update() 
     # Defines game FPS
-    clock.tick(30)
+    clock.tick(FPS)
 
 
 pygame.quit()

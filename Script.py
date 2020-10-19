4428b1b8283ddc9b7a7fdad8be424fc37caa74e2
Chase Johnson <Chase.b.johnson@gmail.com>
2020-10-18 21:45:41 -0700

    finished defining key movement events.

--- Script.py
+++ Script.py
@@ -18,22 +18,40 @@ gameExit = False
 lead_x = 300
 lead_y = 300
 
+lead_x_change = 0
+lead_y_change = 0
+
+clock = pygame.time.Clock()
+
+# Main game loop
 while not gameExit:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             gameExit = True
         if event.type == pygame.KEYDOWN:
             if event.key == pygame.K_LEFT:
-                lead_x -= 10
-            if event.key == pygame.K_RIGHT:
-                lead_x =+ 10
+                lead_x_change = -10
+                lead_y_change = 0
+            elif event.key == pygame.K_RIGHT:
+                lead_x_change = 10
+                lead_y_change = 0
+            elif event.key == pygame.K_UP:
+                lead_y_change = -10
+                lead_x_change = 0
+            elif event.key == pygame.K_DOWN:
+                lead_y_change = 10
+                lead_x_change = 0
 
-
+    
+    lead_x += lead_x_change
+    lead_y += lead_y_change
     # Defines game background
     gameDisplay.fill(white)
-    pygame.draw.rect(gameDisplay, black, [lead_x, Lead_y, 10, 10])
+    pygame.draw.rect(gameDisplay, black, [lead_x,lead_y,10,10])
     # Updates game display
     pygame.display.update() 
+    # Defines game FPS
+    clock.tick(15)
 
 
 pygame.quit()

b98adfcc8feeb5d45bf81714f1dd404cd6fbe703
Chase Johnson <Chase.b.johnson@gmail.com>
2020-10-18 21:16:40 -0700

    Added game display color.

--- Script.py
+++ Script.py
@@ -3,6 +3,9 @@ import pygame
 # Starts game 
 pygame.init()
 
+white = (255,255,255)
+black = (0,0,0)
+
 # Game display window 
 gameDisplay = pygame.display.set_mode((800,600))
 
@@ -15,8 +18,11 @@ gameExit = False
 while not gameExit:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
-            gameExit = True 
+            gameExit = True
 
-# quits game 
-pygame.quit()
-quit()
+    gameDisplay.fill(white)
+    pygame.draw.rect(gameDisplay, black, [400, 300, 10, 10])
+
+    pygame.display.update() 
+
+

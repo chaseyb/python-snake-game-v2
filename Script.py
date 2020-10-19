0e67f818d5bef01c8167259d6e3468f7ca0eb895
Chase Johnson <Chase.b.johnson@gmail.com>
2020-10-18 20:56:15 -0700

    added game title.

--- Script.py
+++ Script.py
@@ -8,6 +8,14 @@ gameDisplay = game.display.set_mode((800,600))
 
 #updates game
 pygame.display.update()
+pygame.display.set_caption('Snake Game V2 by Chaseyb')
+
+gameExit = False
+
+while not gameExit:
+    for event in pygame.event.get():
+        print(event)
 
 # quits game 
 pygame.quit()
+quit()

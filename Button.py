94ecb9fbc3092ccfe3ad39e09775dabd5aab5d6c
Chase Johnson <Chase.b.johnson@gmail.com>
2020-10-20 18:42:38 -0700

    created button class.

---
+++ Button.py
@@ -0,0 +1,5 @@
+class button(object):
+    """
+    This is a button class that makes it easy to create buttons. It includes a button initializer, and several control
+    functions, such as changing colour when the button is hovered.
+    """
